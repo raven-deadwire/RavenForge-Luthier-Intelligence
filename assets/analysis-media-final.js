e4ed44e7c72228bca658118e51c6238cc9906726
raven-deadwire <memory_64085@naver.com>
2026-09-14 08:15:54 +0900

    Use HTTPS for Spector NS-2 image

--- assets/analysis-media-final.js
+++ assets/analysis-media-final.js
@@ -5,7 +5,7 @@ window.RF_ANALYSIS_EXTRA_MEDIA=Object.assign(window.RF_ANALYSIS_EXTRA_MEDIA||{},
   {label:'Essential 5 — Mike Pope',src:'https://www.luthiersaccessgroup.com/wp-content/uploads/2012/12/pi_472_3162_p1130189-535x350.jpg',pos:'50% 60%'}
  ],source:'https://www.luthiersaccessgroup.com/basses/bass-builders-profile/zakrzewski-basses/'},
  'Spector Basses':{model:'NS-2 / NS-5',shots:[
-  {label:'NS-2',src:'http://spectorbass.com/cdn/shop/files/Custom_Shop_Basses_2025_0057_224592C.webp?v=1757950781&width=400',pos:'50% 62%'},
+  {label:'NS-2',src:'https://spectorbass.com/cdn/shop/files/Custom_Shop_Basses_2025_0057_224592C.webp?v=1757950781&width=400',pos:'50% 62%'},
   {label:'NS-5',src:'https://spectorbass.com/cdn/shop/files/Custom_Shop_Basses_2024_0085_223081C.webp?v=1757967530&width=400',pos:'50% 62%'}
  ],source:'https://spectorbass.com/'}
 });
