eb8e15041044b25bb23849deadbaeea65089a2a2
raven-deadwire <memory_64085@naver.com>
2026-09-14 07:17:38 +0900

    Add Zakrzewski representative media

---
+++ assets/analysis-media-final.js
@@ -0,0 +1,8 @@
+(()=>{'use strict';
+window.RF_ANALYSIS_EXTRA_MEDIA=Object.assign(window.RF_ANALYSIS_EXTRA_MEDIA||{}, {
+ 'Zakrzewski Basses':{model:'Essential 5 / Essential 5 — Mike Pope',shots:[
+  {label:'Essential 5 — Aero / Aguilar',src:'https://www.luthiersaccessgroup.com/wp-content/uploads/2012/10/pi_504_3420_p1140518-535x350.jpg',pos:'50% 60%'},
+  {label:'Essential 5 — Mike Pope',src:'https://www.luthiersaccessgroup.com/wp-content/uploads/2012/12/pi_472_3162_p1130189-535x350.jpg',pos:'50% 60%'}
+ ],source:'https://www.luthiersaccessgroup.com/basses/bass-builders-profile/zakrzewski-basses/'}
+});
+})();
