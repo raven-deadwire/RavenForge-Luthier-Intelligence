501a8678c36715bdec7ea64047e6a9afca0b27a6
raven-deadwire <memory_64085@naver.com>
2026-09-14 08:02:32 +0900

    Curate representative models and body-focused crops

---
+++ assets/analysis-media-curation.js
@@ -0,0 +1,49 @@
+(()=>{'use strict';
+const M=window.RF_ANALYSIS_EXTRA_MEDIA=window.RF_ANALYSIS_EXTRA_MEDIA||{};
+Object.assign(M,{
+ 'Anaconda Basses':{model:'Ultra J5 / Ultra PJ5',shots:[
+  {label:'Ultra J5',src:'https://lirp.cdn-website.com/7604c249/dms3rep/multi/opt/UJ5%2BJM%2BFull%2BHorz-1920w.JPG',pos:'50% 56%',zoom:1.18,origin:'50% 56%'},
+  {label:'Ultra PJ5',src:'https://img07.shop-pro.jp/PA01348/005/product/185959373.jpg?cmsp_timestamp=20250405142425',pos:'50% 66%',zoom:1.18,origin:'50% 66%'}
+ ],source:'https://www.anacondabasses.co.uk/'},
+ 'Maruszczyk Instruments':{model:'Elwood / Jake',shots:[
+  {label:'Elwood',src:'https://thebassgallery.com/cdn/shop/products/image_9fa6cb1c-16c8-48f7-ae43-e1a4c32bd65a_1600x.jpg?v=1641392596',pos:'50% 72%',zoom:1.22,origin:'50% 72%'},
+  {label:'Jake',src:'https://bassfreaks.net/cdn/shop/files/IMG_0322.jpg?v=1682799715',pos:'50% 70%',zoom:1.2,origin:'50% 70%'}
+ ],source:'https://www.public-peace.de/'},
+ 'Wal Basses':{model:'Mk1 / Mk2',shots:[
+  {label:'Mk1',src:'https://thebassgallery.com/cdn/shop/files/CF83CCF4-C248-4C61-8427-81AB4E8524E3_1600x.jpg?v=1758801987',pos:'50% 73%',zoom:1.27,origin:'50% 73%'},
+  {label:'Mk2',src:'https://thebassgallery.com/cdn/shop/files/IMG_0687_1600x.jpg?v=1686226465',pos:'50% 72%',zoom:1.27,origin:'50% 72%'}
+ ],source:'https://walbasses.co.uk/'},
+ 'Alusonic Aluminium Instruments':{model:'Django Supreme Carbon / J-Special Supreme 5',shots:[
+  {label:'Django Supreme Carbon',src:'https://www.alusonic.com/img/djsc/vertical.JPG',pos:'50% 70%',zoom:1.12,origin:'50% 70%'},
+  {label:'J-Special Supreme 5',src:'https://www.alusonic.com/img/jspsp5/vertical.jpg',pos:'50% 70%',zoom:1.12,origin:'50% 70%'}
+ ],source:'https://www.alusonic.com/aluminium-bass-django-supreme-carbon.php'},
+ 'Aviator Guitars':{model:'Jetstream Bass 5 / Jetstream Bass 6',shots:[
+  {label:'Jetstream Bass 5',src:'https://www.aviator-guitars.com/images/pilot/mockups/bass_modern/5%202.webp',fallback:'https://www.aviator-guitars.com/images/pilot/mockups/bass_modern/5%201.webp',pos:'50% 70%',zoom:1.28,origin:'50% 70%'},
+  {label:'Jetstream Bass 6',src:'https://shop.aviator-guitars.com/cdn/shop/files/Screenshot2025-10-02at19.46.00.png?v=1759427180',fallback:'https://www.aviator-guitars.com/images/pilot/mockups/bass_modern/6%202.webp',pos:'50% 67%',zoom:1.15,origin:'50% 67%'}
+ ],source:'https://www.aviator-guitars.com/home/pilotmodernbass/'},
+ 'Oroboro Guitars':{model:'Boa 5 / Python 5',shots:[
+  {label:'Boa 5',src:'https://oroboroguitars.com/cdn/shop/files/0_f0864fa0-01a5-4c4b-9a3f-596f5c3e5bd8.jpg?v=1688063981',pos:'50% 63%',zoom:1.08,origin:'50% 63%'},
+  {label:'Python 5',src:'https://oroboroguitars.com/cdn/shop/files/0_6_8873d84d-e075-445d-bcc5-e47f58df43d2.jpg?v=1688064200&width=1946',pos:'50% 63%',zoom:1.08,origin:'50% 63%'}
+ ],source:'https://oroboroguitars.com/en/collections/electric-basses'},
+ 'Zakrzewski Basses':{model:'Essential / Superior',shots:[
+  {label:'Essential 5',src:'https://www.luthiersaccessgroup.com/wp-content/uploads/2012/10/pi_504_3420_p1140518-535x350.jpg',pos:'50% 58%',zoom:1.12,origin:'50% 58%'},
+  {label:'Superior 5 SC',src:'https://www.talkbass.com/classifieds/zakrzewski-superior-5-sc-rare.1686436/cover-image',fallback:'https://www.luthiersaccessgroup.com/wp-content/uploads/2012/12/pi_472_3162_p1130189-535x350.jpg',pos:'50% 62%',zoom:1.12,origin:'50% 62%'}
+ ],source:'https://www.talkbass.com/classifieds/zakrzewski-superior-5-sc-rare.1686436/'},
+ 'Halo Custom Guitars':{model:'Octavia 5 Multiscale / MERUS 6 Headless',shots:[
+  {label:'Octavia 5 Multiscale',src:'https://www.haloguitars.com/store/image/cache/catalog/Halo-OCTAVIA-5-MS-Bassb-1200x1200.jpg',fallback:'https://rvb-img.reverb.com/image/upload/s--ffViC94y--/a_0/t_card-square/v1684948130/rxrrrf8unulux2io62vt.jpg',pos:'50% 58%',zoom:1.32,origin:'50% 58%'},
+  {label:'MERUS 6 Headless',src:'https://www.haloguitars.com/store/image/cache/catalog/stock/guitars/HEADLESS-MERUS-6-R-26-HIPSHOT-POPLAR.BURL-TCR-HH/halo-custom-guitars-headless-merus-6-body-1200x1200.jpg',pos:'50% 63%',zoom:1.18,origin:'50% 63%'}
+ ],source:'https://www.haloguitars.com/'},
+ 'Padalka Guitars':{model:'Ennea №156 / Ennea-H SN236',shots:[
+  {label:'Ennea №156',src:'https://www.padalka-guitars.com/web/image/517-e6d262cb/4I2A2036.webp',fallback:'https://pzgnnyvyrmlfxdsgoiji.supabase.co/storage/v1/object/public/instrument-images/catalog/036721c477ef-4I2A6680.webp',pos:'50% 66%',zoom:1.16,origin:'50% 66%'},
+  {label:'Ennea-H SN236',src:'https://pzgnnyvyrmlfxdsgoiji.supabase.co/storage/v1/object/public/instrument-images/catalog/c09628122fda-DSCF2869.webp',pos:'50% 64%',zoom:1.16,origin:'50% 64%'}
+ ],source:'https://www.padalka-guitars.com/ennea#ennea-156'},
+ 'Dingwall Guitars':{model:'Z3 / D-Roc Hellboy',shots:[
+  {label:'Z3',src:'https://assets.bassbuddha.com/app/uploads/2022/01/Dingwall-Z3-Arbutus-Burl-1.jpg',fallback:'https://ish.guitars/cdn/shop/files/7733-6_1200x1200.jpg?v=1735849234',pos:'50% 66%',zoom:1.08,origin:'50% 66%'},
+  {label:'D-Roc Hellboy',src:'https://dingwallguitars.com/wp-content/uploads/2022/07/D-Roc-Hellboy-Vertical-Shot-1467x2200.jpg',fallback:'https://sc1.musik-produktiv.com/pic-010150521xl/dingwall-d-roc-hellboy-limited.jpg',pos:'50% 72%',zoom:1.22,origin:'50% 72%'}
+ ],source:'https://dingwallguitars.com/bass/d-roc-hellboy/'},
+ 'Overwater Basses':{model:'Progress Series IV Custom / CONTEMPORARY J Custom',shots:[
+  {label:'Progress Series IV Custom',src:'https://bassfreaks.net/cdn/shop/files/OverwaterProgressSeriesIVCustom530_-Bubinga0BassFreaks1.jpg?v=1763551684&width=940',fallback:'https://thebassgallery.com/cdn/shop/files/7AD8358C-B7CA-4FC4-88A3-15DFA50EE289_1600x.jpg?v=1715697390',pos:'50% 60%',zoom:1.12,origin:'50% 60%'},
+  {label:'CONTEMPORARY J Custom',src:'https://overwaterbasses.com/wp-content/uploads/2026/06/Contemporary-J-4278.png',pos:'50% 60%',zoom:1.1,origin:'50% 60%'}
+ ],source:'https://overwaterbasses.com/'}
+});
+})();
