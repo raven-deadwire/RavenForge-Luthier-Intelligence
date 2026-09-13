9c617e287e8601e970632c5b6cd3204e41f8607a
raven-deadwire <memory_64085@naver.com>
2026-09-14 07:27:23 +0900

    Use selected Moon Guitar Instagram builds as representative media

--- assets/analysis-moon-media.js
+++ assets/analysis-moon-media.js
@@ -1,1 +1,15 @@
-(()=>{'use strict';window.RF_MOON_POSTS=['https://www.instagram.com/p/DWkycGrEauM/','https://www.instagram.com/p/DWs-e1EEXbR/'];})();
+(()=>{'use strict';
+const posts=['https://www.instagram.com/p/DWkycGrEauM/','https://www.instagram.com/p/DWs-e1EEXbR/'];
+const mediaUrl=u=>u.replace(/\/$/,'')+'/media/?size=l';
+window.RF_MOON_POSTS=posts;
+window.RF_ANALYSIS_EXTRA_MEDIA=Object.assign(window.RF_ANALYSIS_EXTRA_MEDIA||{}, {
+ 'Moon Guitar — Chanho Moon':{
+   model:'Custom Build I / Custom Build II',
+   shots:[
+    {label:'Moon Guitar Custom Build I',src:mediaUrl(posts[0]),pos:'50% 58%'},
+    {label:'Moon Guitar Custom Build II',src:mediaUrl(posts[1]),pos:'50% 58%'}
+   ],
+   source:posts[0]
+ }
+});
+})();
