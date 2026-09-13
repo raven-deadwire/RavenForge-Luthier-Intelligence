f19df5b4f6356e5c94bddb83d3f445981e8ad9b2
raven-deadwire <memory_64085@naver.com>
2026-09-14 07:39:20 +0900

    Fix broken representative model images

---
+++ assets/analysis-media-hotfix.js
@@ -0,0 +1,29 @@
+(()=>{'use strict';
+const M=window.RF_ANALYSIS_EXTRA_MEDIA=window.RF_ANALYSIS_EXTRA_MEDIA||{};
+Object.assign(M,{
+ 'KD Basses':{model:'Picasso / REV II Multiscale',shots:[
+  {label:'Picasso',src:'https://res.cloudinary.com/reverb-cms/image/upload/v1497370515/backfilled/curation/Picasso_yejpwa.jpg',pos:'50% 54%'},
+  {label:'REV II Multiscale',src:'https://www.kdbasses.com/wp-content/uploads/2020/04/Fannedfred-rev-2-Diff.jpg',pos:'50% 61%'}
+ ],source:'https://www.kdbasses.com/'},
+ 'Dingwall Guitars':{model:'Z3 / D-Roc',shots:[
+  {label:'Z3',src:'https://assets.bassbuddha.com/app/uploads/2022/01/Dingwall-Z3-Arbutus-Burl-1.jpg',fallback:'https://ish.guitars/cdn/shop/files/7733-6_1200x1200.jpg?v=1735849234',pos:'50% 64%'},
+  {label:'D-Roc',src:'https://www.andybaxterbass.com/cdn/shop/files/2S5A0117.jpg?v=1756821458',fallback:'https://sc1.musik-produktiv.com/pic-010166581xxl/dingwall-d-roc-standard-4-dw.jpg',pos:'50% 55%'}
+ ],source:'https://dingwallguitars.com/'},
+ 'Lorita Basses':{model:'Century / Century Series II',shots:[
+  {label:'Century',src:'https://www.doctorbass.net/imagftp/im21005173113lorita-andre769s-rotmistrovsky-signature.jpg',fallback:'https://www.talkbass.com/classifieds/lorita-century-5-hollow-body-5-24-33.1620511/cover-image',pos:'50% 61%'},
+  {label:'Century Series II',src:'https://rvb-img.reverb.com/image/upload/s--MY59dzJK--/a_0/f_auto%2Ct_large/v1721778387/bnp6qyviw3qjrtd3w9o0.jpg',fallback:'https://www.talkbass.com/media/lorita-century.10138/full?d=1465940124',pos:'50% 54%'}
+ ],source:'https://www.talkbass.com/threads/lorita-basses-new-model.1305507/'},
+ 'Overwater Basses':{model:'Progress Series IV Custom / Progress IV',shots:[
+  {label:'Progress Series IV Custom',src:'https://bassfreaks.net/cdn/shop/files/OverwaterProgressSeriesIVCustom530_-Bubinga0BassFreaks1.jpg?v=1763551684&width=940',fallback:'https://thebassgallery.com/cdn/shop/files/7AD8358C-B7CA-4FC4-88A3-15DFA50EE289_1600x.jpg?v=1715697390',pos:'50% 55%'},
+  {label:'Progress IV',src:'https://images.squarespace-cdn.com/content/v1/67638e13cfbacf611f495e15/83016b39-ee39-40b5-90f1-a20d728a894d/20250817_220951.jpg?format=1000w',fallback:'https://bassfreaks.net/cdn/shop/files/OverwaterProgressSeriesIVCustom530_-Bubinga0BassFreaks1.jpg?v=1763551684&width=940',pos:'50% 60%'}
+ ],source:'https://overwaterbasses.com/'},
+ 'Padalka Guitars':{model:'Ennea / Ennea NAMM',shots:[
+  {label:'Ennea',src:'https://www.chicagomusicexchange.com/cdn/shop/products/padalka-padalka-ennea-bgs20-padalka-1-17401445318791.jpg?v=1648043045&width=1946',fallback:'https://www.makenmusic.com/cdn/shop/products/Simon_Padalka_Guitars_ENNEA-Bass_Black_4-string_Custom_Boutique_Electric_FBV.jpg?v=1587242600',pos:'50% 57%'},
+  {label:'Ennea NAMM',src:'https://www.makenmusic.com/cdn/shop/products/Simon_Padalka_Guitars_ENNEA-Bass_Black_4-string_Custom_Boutique_Electric_FB.jpg?v=1587242600',fallback:'https://www.chicagomusicexchange.com/cdn/shop/products/padalka-padalka-ennea-bgs20-padalka-1-17401445318791.jpg?v=1648043045&width=1946',pos:'50% 60%'}
+ ],source:'https://www.padalka-guitars.com/ennea'},
+ 'Alembic Inc.':{model:'Series I / Series II',shots:[
+  {label:'Series I',src:'https://www.alembicguitars.com/assets/images/DET_SIB_amber.png',pos:'50% 55%'},
+  {label:'Series II',src:'https://www.alembicguitars.com/assets/images/DET_SIIB_cocoboloswirlL.png',fallback:'https://i0.wp.com/www.fretspot.com/wp-content/uploads/2024/04/20240419_101655.jpg?fit=1496%2C1496&ssl=1',pos:'50% 55%'}
+ ],source:'https://www.alembicguitars.com/seriesiibass.html'}
+});
+})();
