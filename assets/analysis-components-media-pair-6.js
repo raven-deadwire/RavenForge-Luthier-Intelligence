80510ff741239152c909539becaf49a35641d978
raven-deadwire <memory_64085@naver.com>
2026-09-14 23:25:27 +0900

    Use visible Spartan 2 image source

--- assets/analysis-components-media-pair-6.js
+++ assets/analysis-components-media-pair-6.js
@@ -6,7 +6,7 @@ Object.assign(window.RF_ANALYSIS_EXTRA_MEDIA=window.RF_ANALYSIS_EXTRA_MEDIA||{},
  ],source:'https://www.eich-amps.com/'},
  'Vanderkley Amplification':{pair:true,model:'Aurora / Spartan 2',shots:[
   {label:'Aurora',src:'https://rvb-img.reverb.com/i/s--GFVZwzZe--/quality%3Dmedium-low%2Cheight%3D800%2Cwidth%3D800%2Cfit%3Dcontain/mswatk31fk0blormthfe.jpg',fallback:'https://images.equipboard.com/uploads/item/image/50775/vanderkley-aurora-bass-head-s.webp?v=1754869121',pos:'50% 50%',zoom:1.02,origin:'50% 50%',fit:'cover',modalFit:'contain',modalZoom:1},
-  {label:'Spartan 2',src:'https://www.bassic.de/attachments/img_3912-webp.847276/',fallback:'https://rvb-img.reverb.com/i/s--qZstuDao--/quality%3Dmedium-low%2Cheight%3D800%2Cwidth%3D800%2Cfit%3Dcontain/lqzerrd0lwnwfvpyzbqt.jpg',pos:'50% 50%',zoom:1,origin:'50% 50%',fit:'contain',modalFit:'contain',modalPos:'50% 50%',modalZoom:0.88}
+  {label:'Spartan 2',src:'https://www.talkbass.com/attachments/spartan-2-jpg.4036715/',fallback:'https://rvb-img.reverb.com/i/s--qZstuDao--/quality%3Dmedium-low%2Cheight%3D800%2Cwidth%3D800%2Cfit%3Dcontain/lqzerrd0lwnwfvpyzbqt.jpg',pos:'50% 50%',zoom:1,origin:'50% 50%',fit:'contain',modalFit:'contain',modalPos:'50% 50%',modalZoom:0.92}
  ],source:'https://vanderkley.blogspot.com/'}
 });
 })();
