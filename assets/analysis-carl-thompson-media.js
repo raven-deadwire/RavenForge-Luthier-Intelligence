624c8a7b3cb2beb58b545e5dea5c55889b3d9f46
raven-deadwire <memory_64085@naver.com>
2026-09-15 01:56:07 +0900

    Add Carl Thompson representative images

---
+++ assets/analysis-carl-thompson-media.js
@@ -0,0 +1,8 @@
+(()=>{'use strict';
+Object.assign(window.RF_ANALYSIS_EXTRA_MEDIA=window.RF_ANALYSIS_EXTRA_MEDIA||{}, {
+ 'Carl Thompson':{pair:true,model:'6-String Scroll Bass / 4-String Scroll Bass',shots:[
+  {label:'6-String Scroll Bass',src:'https://ctbasses.com/images/6/12-20-20/12-20-20_015.JPG',fallback:'https://www.mikigakki.com/ec/bass/MBS-69K008_S_2m.jpg',pos:'50% 58%',zoom:1.08,origin:'50% 58%',fit:'cover',modalFit:'contain',modalPos:'50% 50%',modalZoom:1},
+  {label:'4-String Scroll Bass',src:'https://ctbasses.com/images/4/4-16-09/4-16-09_013.jpg',fallback:'https://www.russomusic.com/cdn/shop/files/ISS14513CCarlThompsonUsed1991Custom4StringWOgb-3_1024x.jpg?v=1694437432',pos:'50% 56%',zoom:1.06,origin:'50% 56%',fit:'cover',modalFit:'contain',modalPos:'50% 50%',modalZoom:1}
+ ],source:'https://ctbasses.com/'}
+});
+})();
