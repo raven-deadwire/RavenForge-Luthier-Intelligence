d1d3731552860849d8308c0eb0b62841d7280691
raven-deadwire <memory_64085@naver.com>
2026-09-14 23:25:58 +0900

    Refresh analysis media cache for Spartan 2

--- assets/analysis-review.js
+++ assets/analysis-review.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const v='20260914s17';
+const v='20260914s18';
 const load=src=>new Promise(done=>{const s=document.createElement('script');s.src=src;s.async=false;s.onload=s.onerror=done;document.body.appendChild(s)});
 const files=[
 'analysis-special-entities.js','analysis-special-nova.js','analysis-special-zuta.js','analysis-special-components.js','analysis-special-components-2.js','analysis-special-amplification.js','analysis-technology-cleanup.js','analysis-review-core.js','analysis-degier.js','analysis-structure-normalize.js','analysis-amplification-review.js','analysis-technology-sources.js','analysis-amplification-sources.js','analysis-media.js','analysis-media-extra.js','analysis-components-media.js','analysis-components-media-pair-1.js','analysis-components-media-pair-2.js','analysis-components-media-pair-3.js','analysis-components-media-pair-4.js','analysis-components-media-pair-5.js','analysis-components-media-pair-6.js','analysis-moon-media.js','analysis-media-hotfix.js','analysis-media-curation.js','analysis-media-final.js','analysis-media-extra-render.js','analysis-order.js'
