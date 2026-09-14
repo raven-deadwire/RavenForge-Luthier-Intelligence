c1b5bf2f1776375e54d9b94b845f278594da5d22
raven-deadwire <memory_64085@naver.com>
2026-09-14 09:23:01 +0900

    Add paired media for Turner and Herrick

---
+++ assets/analysis-components-media-pair-2.js
@@ -0,0 +1,12 @@
+(()=>{'use strict';
+Object.assign(window.RF_ANALYSIS_EXTRA_MEDIA=window.RF_ANALYSIS_EXTRA_MEDIA||{}, {
+ 'Turner Pickups':{pair:true,model:'MC4 Multicoil / MC5 Multicoil Set',shots:[
+  {label:'MC4 Multicoil',src:'https://rvb-img.reverb.com/image/upload/s--Ah6_kPVt--/f_auto,t_large/v1644095315/o5grdukqrecblxxywnvi.jpg',pos:'50% 50%',zoom:1.03,origin:'50% 50%',fit:'cover'},
+  {label:'MC5 Multicoil Set',src:'https://www.public-peace.de/images/product_images/info_images/Shopbilder/Turner%20Pickups/MC5X18.png',pos:'50% 50%',zoom:1.03,origin:'50% 50%',fit:'cover'}
+ ],source:'https://www.turnerpickups.com/'},
+ 'Herrick Pickups':{pair:true,model:'8-Coil MM Multicoil / J4 Soapbar Multicoil',shots:[
+  {label:'8-Coil MM Multicoil',src:'https://image.jimcdn.com/app/cms/image/transf/dimension=1920x10000:format=jpg/path/s12a29c6b7100d4e8/image/i42bf2a6aae7a0689/version/1718438302/image.jpg',pos:'50% 50%',zoom:1.05,origin:'50% 50%',fit:'cover'},
+  {label:'J4 Soapbar Multicoil',src:'https://image.jimcdn.com/app/cms/image/transf/dimension=1920x10000:format=jpg/path/s12a29c6b7100d4e8/image/ib96748cb10659229/version/1671299442/image.jpg',pos:'50% 50%',zoom:1.05,origin:'50% 50%',fit:'cover'}
+ ],source:'https://www.herrickpickups.com/'}
+});
+})();
