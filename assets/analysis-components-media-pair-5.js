7b80854bc6c2c6505cf41caaae9f0da29399680d
raven-deadwire <memory_64085@naver.com>
2026-09-14 21:53:33 +0900

    Add Delano and Häussel component media

---
+++ assets/analysis-components-media-pair-5.js
@@ -0,0 +1,12 @@
+(()=>{'use strict';
+Object.assign(window.RF_ANALYSIS_EXTRA_MEDIA=window.RF_ANALYSIS_EXTRA_MEDIA||{}, {
+ 'Delano Pickup Systems':{pair:true,model:'JMVC 4 FE/M2 / The Xtender 5 HE/S',shots:[
+  {label:'JMVC 4 FE/M2',src:'https://www.bassparts.de/WebRoot/Store/Shops/61038859/4A71/4EA1/6E8C/A3BE/503C/C0A8/2935/A589/jmvc4_fe_m2.png',pos:'50% 50%',zoom:1.03,origin:'50% 50%',fit:'cover'},
+  {label:'The Xtender 5 HE/S',src:'https://rvb-img.reverb.com/image/upload/s--5XbG2J91--/f_auto%2Ct_large/v1658246778/geit8pltazk2lphe3os8.jpg',fallback:'https://fatbasstone.com/cdn/shop/products/delano-thextender-5-he_s-bass-pickup-back-side-1024x-wm_2048x.jpg?v=1639438102',pos:'50% 50%',zoom:1.02,origin:'50% 50%',fit:'cover'}
+ ],source:'https://www.delano.de/'},
+ 'Häussel Pickups':{pair:true,model:'MM 5-string / JB 5-string 70ies',shots:[
+  {label:'MM 5-string',src:'https://www.bassparts.de/WebRoot/Store/Shops/61038859/5A0D/970C/C14E/F577/C5F3/0A0C/6D05/795D/MM5_ohne_Ohren_-_black.jpg',pos:'50% 50%',zoom:1.03,origin:'50% 50%',fit:'cover'},
+  {label:'JB 5-string 70ies',src:'https://www.bassparts.de/WebRoot/Store/Shops/61038859/6409/C790/E40F/83A3/8156/0A0C/6D0F/CE80/003.jpg',pos:'50% 50%',zoom:1.03,origin:'50% 50%',fit:'cover'}
+ ],source:'https://www.haeussel.com/'}
+});
+})();
