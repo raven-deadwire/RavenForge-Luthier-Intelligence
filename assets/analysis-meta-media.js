0b3ef4a25a378c120f29d8ff9bb55e97d9e1ce82
raven-deadwire <memory_64085@naver.com>
2026-09-15 23:30:01 +0900

    Add Meta Guitars Explorer media

---
+++ assets/analysis-meta-media.js
@@ -0,0 +1,8 @@
+(()=>{'use strict';
+Object.assign(window.RF_ANALYSIS_EXTRA_MEDIA=window.RF_ANALYSIS_EXTRA_MEDIA||{}, {
+ 'Meta Guitars':{pair:true,model:'Veil B5 / Pragma Sutkin Signature',shots:[
+  {label:'Veil B5 Midscale',src:'https://assets.bassbuddha.com/app/uploads/2024/11/Meta-Guitars-Veil-B-5-Midscale-Almond-1.jpg',fallback:'https://bassshop-gib.com/cdn/shop/files/me4246-1-1.jpg?v=1703661656',pos:'50% 58%',zoom:1.06,origin:'50% 58%',fit:'cover',modalFit:'contain',modalPos:'50% 50%',modalZoom:1},
+  {label:'Pragma Sutkin Signature',src:'https://i0.wp.com/www.notreble.com/wp-content/uploads/2025/10/Meta-Guitars-Pragma-Sutkin-Body.jpg?resize=1200%2C630&ssl=1',fallback:'https://metaguitars.fr/wp-content/uploads/2025/06/Sutkin-Signature-Pragma-4M-scaled.jpg',pos:'50% 54%',zoom:1.05,origin:'50% 54%',fit:'cover',modalFit:'contain',modalPos:'50% 50%',modalZoom:1}
+ ],source:'https://metaguitars.fr/basses/'}
+});
+})();
