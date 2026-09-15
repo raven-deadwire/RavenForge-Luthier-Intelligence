9d26e90b6ffb70ca78f7374b8a522dc5c07801df
raven-deadwire <memory_64085@naver.com>
2026-09-16 03:09:27 +0900

    Add representative images for Darkglass and MTD analysis entries

---
+++ assets/analysis-darkglass-mtd-media.js
@@ -0,0 +1,22 @@
+(()=>{'use strict';
+window.RF_ANALYSIS_EXTRA_MEDIA=Object.assign(window.RF_ANALYSIS_EXTRA_MEDIA||{}, {
+ 'Darkglass Electronics':{
+  pair:true,
+  model:'Alpha·Omega / Alpha·Omega 500',
+  shots:[
+   {label:'Alpha·Omega',src:'https://www.darkglass.com/cdn/shop/files/Darkglass-Alpha-Omega-Gal-4.png?v=1749583648&width=1445',pos:'50% 50%',fit:'contain',modalFit:'contain'},
+   {label:'Alpha·Omega 500',src:'https://www.darkglass.com/cdn/shop/files/Darkglass-Alpha-Omega-500-Gal-3.png?v=1746051866&width=1445',pos:'50% 50%',fit:'contain',modalFit:'contain'}
+  ],
+  source:'https://www.darkglass.com/'
+ },
+ 'Michael Tobias Design (MTD)':{
+  pair:true,
+  model:'535-24 / USA 535',
+  shots:[
+   {label:'535-24',src:'https://images.squarespace-cdn.com/content/v1/52db2345e4b02995f7e0a10f/c50f3c40-bae4-49fc-b49b-5570205efb98/FullSizeRender.jpeg',pos:'50% 54%',fit:'cover',modalFit:'contain'},
+   {label:'USA 535',src:'https://rvb-img.reverb.com/image/upload/s--6MHPtqf5--/t_card-square/v1652310904/jz9pr5grjouv2snwer22.png',pos:'50% 50%',fit:'contain',modalFit:'contain'}
+  ],
+  source:'https://www.mtdbass.com/handmade-options'
+ }
+});
+})();
