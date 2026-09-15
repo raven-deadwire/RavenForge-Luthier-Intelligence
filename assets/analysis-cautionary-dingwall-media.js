42a30ba632b1cd1cc27a3949052e06a4236e7c32
raven-deadwire <memory_64085@naver.com>
2026-09-15 23:54:40 +0900

    Add Dingwall cautionary case media

---
+++ assets/analysis-cautionary-dingwall-media.js
@@ -0,0 +1,8 @@
+(()=>{'use strict';
+Object.assign(window.RF_ANALYSIS_EXTRA_MEDIA=window.RF_ANALYSIS_EXTRA_MEDIA||{}, {
+ 'Dingwall — Ready-to-Play Premiumization':{pair:true,model:'NG3 / SP1',shots:[
+  {label:'NG3 5-String',src:'https://dingwallguitars.com/wp-content/uploads/2022/06/R2P-NG3-5-String-Ferrari-Green-Shot-3-3-1467x2200.jpg',fallback:'https://dingwallguitars.com/wp-content/uploads/2022/06/R2P-Combustion-6-String-Vintageburst-Pau-Ferro-FB-3-7.jpg',pos:'50% 72%',zoom:1.02,origin:'50% 72%',fit:'cover',modalFit:'contain',modalPos:'50% 50%',modalZoom:1},
+  {label:'SP1 4-String',src:'https://dingwallguitars.com/wp-content/uploads/2025/05/E_R2P-SP1-4-String-Gloss-Ducati-White-Rosewood-FB-Black-PG-Pano-2200x786.jpg',fallback:'https://dingwallguitars.com/wp-content/uploads/2025/05/E_R2P-SP1-4-String-Vintageburst-Rosewood-FB-Black-PG-Detail-Pickups-2200x1467.jpg',pos:'50% 50%',zoom:1,origin:'50% 50%',fit:'contain',modalFit:'contain',modalPos:'50% 50%',modalZoom:1}
+ ],source:'https://dingwallguitars.com/ready-to-play/'}
+});
+})();
