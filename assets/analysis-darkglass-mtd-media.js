ebf6542097fe301b3d38cb1caea7cb545d4afedb
raven-deadwire <memory_64085@naver.com>
2026-09-16 07:22:54 +0900

    Use square media for Anaconda and Maruszczyk

--- assets/analysis-darkglass-mtd-media.js
+++ assets/analysis-darkglass-mtd-media.js
@@ -17,6 +17,24 @@ window.RF_ANALYSIS_EXTRA_MEDIA=Object.assign(window.RF_ANALYSIS_EXTRA_MEDIA||{},
    {label:'635-24 — Holly 10 Top',src:'https://www.kurosawagakki.com/mtd/img/3909/03.jpg',fallback:'https://www.kurosawagakki.com/mtd/img/3909/01.jpg',pos:'50% 50%',fit:'cover',modalFit:'contain'}
   ],
   source:'https://www.mtdbass.com/from-the-shop/2016/5/25/735-24'
+ },
+ 'Anaconda Basses':{
+  pair:true,
+  model:'Crusher CXE Elite / Ultra J5 Essence',
+  shots:[
+   {label:'Crusher CXE Elite',src:'https://lirp.cdn-website.com/7604c249/dms3rep/multi/opt/Crusher%2BCXE6%2BWeb%2BMulti-Photoroom-1920w.jpg',pos:'50% 50%',fit:'cover',modalFit:'contain'},
+   {label:'Ultra J5 Essence',src:'https://assets.bassbuddha.com/app/uploads/2023/01/Anaconda-Ultra-J5-Essence-Seafoam-Green-PRE-OWNED-1.jpg',pos:'50% 50%',fit:'cover',modalFit:'contain'}
+  ],
+  source:'https://www.anacondabasses.co.uk/crusher-cxe---elite567c9839'
+ },
+ 'Maruszczyk Instruments':{
+  pair:true,
+  model:'Frog Omega 5a / Frog Signature Pi',
+  shots:[
+   {label:'Frog Omega 5a — 2 Tone Blue',src:'https://media.rainpos.com/6727/20220228_113625.jpg',pos:'50% 50%',fit:'cover',modalFit:'contain'},
+   {label:'Frog Signature Pi 4a-33',src:'https://sargsguitars.com/cdn/shop/files/MaruszczykFrogsignaturePi4a-33_0Sarg_sGuitars8.jpg?v=1762600551',pos:'50% 50%',fit:'cover',modalFit:'contain'}
+  ],
+  source:'https://www.public-peace.de/info/frog.html?language=en'
  }
 });
 })();
