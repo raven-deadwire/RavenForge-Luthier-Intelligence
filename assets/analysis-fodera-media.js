87066741e3e9dee2110a5cd0886e7e90a68ad622
raven-deadwire <memory_64085@naver.com>
2026-09-16 00:36:02 +0900

    Add Fodera representative media

---
+++ assets/analysis-fodera-media.js
@@ -0,0 +1,8 @@
+(()=>{'use strict';
+Object.assign(window.RF_ANALYSIS_EXTRA_MEDIA=window.RF_ANALYSIS_EXTRA_MEDIA||{}, {
+ 'Fodera Guitars':{pair:true,model:'Monarch 4 Standard / Emperor 6 Standard',shots:[
+  {label:'Monarch 4 Standard Special',src:'https://media.sweetwater.com/m/products/image/7b56fba94fKmbzZgJp3PhigBg9WxfsPrEppqUBHk.jpg?ha=7b56fba94ff870166a19cb2d5bf42883234f1692&height=1600&quality=85',fallback:'https://ties.jp/store/html/upload/save_image/1022200241_653501514bf7f.jpg',pos:'50% 58%',zoom:1.02,origin:'50% 58%',fit:'cover',modalFit:'contain',modalPos:'50% 50%',modalZoom:1},
+  {label:'Emperor 6 Standard Special',src:'https://www.slapstore.us/cdn/shop/files/Slapstore_Fodera_Emperor_6_Standard_Special_Buckeye_Burl_2018_18_6790a94c-7681-4f0b-8728-06429d440c5f.webp?v=1770820585&width=1445',fallback:'https://cdn.long-mcquade.com/files/50721/56f6154de57dc8bb38b17e1d79259cdb.jpg',pos:'50% 56%',zoom:1.02,origin:'50% 56%',fit:'cover',modalFit:'contain',modalPos:'50% 50%',modalZoom:1}
+ ],source:'https://fodera.com/'}
+});
+})();
