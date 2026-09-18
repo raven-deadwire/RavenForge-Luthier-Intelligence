8d3261b678f8a1a80ed0515dc2b22eba0045c070
raven-deadwire <memory_64085@naver.com>
2026-09-19 07:13:58 +0900

    Add McGrath selector media

--- assets/analysis-components-media-pair-5.js
+++ assets/analysis-components-media-pair-5.js
@@ -7,6 +7,10 @@ Object.assign(window.RF_ANALYSIS_EXTRA_MEDIA=window.RF_ANALYSIS_EXTRA_MEDIA||{},
  'Häussel Pickups':{pair:true,model:'MM 5-string / JB 5-string 70ies',shots:[
   {label:'MM 5-string',src:'https://www.bassparts.de/WebRoot/Store/Shops/61038859/5A0D/970C/C14E/F577/C5F3/0A0C/6D05/795D/MM5_ohne_Ohren_-_black.jpg',pos:'50% 50%',zoom:1.03,origin:'50% 50%',fit:'cover'},
   {label:'JB 5-string 70ies',src:'https://www.bassparts.de/WebRoot/Store/Shops/61038859/6409/C790/E40F/83A3/8156/0A0C/6D0F/CE80/003.jpg',pos:'50% 50%',zoom:1.03,origin:'50% 50%',fit:'cover'}
- ],source:'https://www.haeussel.com/'}
+ ],source:'https://www.haeussel.com/'},
+ 'McGrath Guitars — 12-Way Rotary Selector':{pair:true,model:'12-Way Rotary Selector / Companion App',shots:[
+  {label:'12-Way Relay Selector',src:'https://mcgrathguitars.com/cdn/shop/articles/sdrswed_38c754d9-a92e-4acc-b4b2-fb2ab3784a3d.jpg?v=1784932011&width=1100',fallback:'https://fretnation.com/cdn/shop/files/mcgrath-rotary-12.jpg?v=1783283597&width=1946',pos:'50% 50%',zoom:1.01,origin:'50% 50%',fit:'cover',modalFit:'contain',modalZoom:1},
+  {label:'Companion App',src:'https://mcgrathguitars.com/cdn/shop/articles/mcgapp_1d9cc3ee-54b8-4000-9a3a-af41720688e8.png?v=1785632376&width=1100',pos:'50% 50%',zoom:1,origin:'50% 50%',fit:'cover',modalFit:'contain',modalZoom:1}
+ ],source:'https://mcgrathguitars.com/blogs/news/12-way-rotary-selector'}
 });
 })();
