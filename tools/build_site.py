0b414336242472b15a63889763b3111d8f5492e0
raven-deadwire <memory_64085@naver.com>
2026-09-22 18:46:22 +0900

    Add McGrath Guitars as luthier reference

--- tools/build_site.py
+++ tools/build_site.py
@@ -8,7 +8,7 @@ from urllib.parse import urljoin
 SITE = 'https://raven-deadwire.github.io/RavenForge-Luthier-Intelligence/'
 INDEX = Path('index.html')
 CONFIG = Path('configurator.html')
-FOOTER_SCRIPT_VERSION = '20260922s26'
+FOOTER_SCRIPT_VERSION = '20260922s27'
 
 
 def read_preserve(path: Path) -> str:
