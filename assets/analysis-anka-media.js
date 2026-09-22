a063e72bce8a9054d68bc6e2470e8291971f8729
raven-deadwire <memory_64085@naver.com>
2026-09-22 18:39:18 +0900

    Add Anka Custom to analysis and exploration

---
+++ assets/analysis-anka-media.js
@@ -0,0 +1,13 @@
+(()=>{'use strict';
+window.RF_ANALYSIS_EXTRA_MEDIA=Object.assign(window.RF_ANALYSIS_EXTRA_MEDIA||{}, {
+ 'Anka Custom':{
+  pair:true,
+  model:'Hathor Deluxe 5 / Noumeno DX',
+  shots:[
+   {label:'Hathor Deluxe 5',src:'https://i0.wp.com/www.notreble.com/wp-content/uploads/2017/09/Anka-Custom-Guitars-Hathor-Deluxe-5-String-Body-Bass-Body-Angle-3-1500x1000.jpg?resize=1500%2C1000',fallback:'https://i0.wp.com/www.notreble.com/wp-content/uploads/2017/09/Anka-Custom-Guitars-Hathor-Deluxe-5-String-Bass.jpg?fit=960%2C640&ssl=1',pos:'50% 52%',fit:'cover',modalFit:'contain'},
+   {label:'Noumeno DX',src:'https://i0.wp.com/www.notreble.com/wp-content/uploads/2023/11/Anka-Custom-Noumeno-DX-Bass-Body-1000x667.jpeg',fallback:'https://i0.wp.com/www.notreble.com/wp-content/uploads/2023/11/Anka-Custom-Noumeno-DX-Bass.jpeg?resize=1200%2C395&ssl=1',pos:'50% 50%',fit:'cover',modalFit:'contain'}
+  ],
+  source:'https://ankacustom.com/electric-basses/'
+ }
+});
+})();
